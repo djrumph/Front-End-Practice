518e96ea0e3cb44fb0ee6628a660fb457a3bb1ec
DJ <DJrumph@gmail.com>
2018-07-04 00:05:50 -0400

    updates

--- canvasWork.js
+++ canvasWork.js
@@ -41,6 +41,47 @@ controller  = {
     }
 };
 
+loop = function(){
+
+    if (controller.up && rectangle.jumping == false){
+
+        rectangle.y_velocity -= 20;
+        rectangle.jumping = true;
+    }
+
+    if(controller.left){
+        rectangle.x_velocity -= 0.5;
+    }
+
+    if(controller.right){
+        rectangle.x_velocity +=0.5;
+    }
+
+    rectangle.y_velocity += 0.5;// gravity
+    rectangle.x += rectangle.x_velocty;
+    rectangle.y += rectangle.y_velocty;
+    rectangle.x_velocity *= 0.9;//friction
+    rectangle.y_velocity *= 0.9;//friction
+
+    if(rectantle.y >180 -16 -32){
+        rectangle.jumping = false;
+        rectangle.y = 180 -16-32;
+        rectangle.y_velocity = 0;
+
+    }
+
+    if(rectangle.x <-32){
+
+        rectangle.x = 320;
+
+
+    } else if (rectangle.x >320){
+
+        rectangle.x = 32;
+    }
+
+    
+}
 
 
 window.addEventListener("keydown",controller.keyListener);
