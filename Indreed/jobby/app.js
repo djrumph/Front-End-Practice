2ff7e7a386807eca06ac57903d3cdc4c96fd7730
DJ <DJrumph@gmail.com>
2018-07-23 23:39:12 -0400

    adding sort query

--- Indreed/jobby/app.js
+++ Indreed/jobby/app.js
@@ -1,4 +1,3 @@
-
 const axios = require('axios');
 const express = require('express');
 const app = express();
@@ -15,14 +14,15 @@ app.use(express.static(path.join(__dirname, 'assets')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', '.hbs');
 
-
-
+app.get('/', function (req, res) {
+    res.render("index", {title:"Jobby"});
+});
 
 app.get('/search', function (req, res) {
     queries = req.query;
     let url = `https://indreed.herokuapp.com/api/jobs`;
     if (queries){
-    axios.get(url, {
+        axios.get(url, {
         params: queries
     })
     .then(function(response){
@@ -36,7 +36,6 @@ app.get('/search', function (req, res) {
         res.render("search", {title: "Jobby"})
     }
 });
-
 
 app.listen(app.get('PORT'), function () {
     console.log('Express started on http://localhost:' +
